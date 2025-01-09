83a31eae8c533a72450d43eab3c80e8439736d4c
rindo57 <77073061+rindo57@users.noreply.github.com>
2025-01-09 18:40:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,4 +24,4 @@ def mirror(id):
         return f"An error occurred: {str(e)}", 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', host=80, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
