b804425cb5c81fb48a981d80409eacd7c40b8538
rindo57 <77073061+rindo57@users.noreply.github.com>
2025-01-09 19:56:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def mirror(id):
             # Find and update the CSS link tag
             css_link = soup.find('link', rel='stylesheet')
             if css_link:
-                css_link['href'] = '/pizza.css'
+                css_link['href'] = '/static/pizza.css'
             
             # Find and remove the "Anime Tosho" link
             header_left = soup.find('span', id='header_left')
