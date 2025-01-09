be3dbc7fa4ea2400bc670d8ccece3f661ed97b90
rindo57 <77073061+rindo57@users.noreply.github.com>
2025-01-09 19:04:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,16 +25,17 @@ def mirror(id):
             
             # Replace GitHub icon with Telegram icon in the header
             telegram_icon_svg = '''
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/512px-Telegram_logo.svg.png?20220101141644" alt="Telegram Icon" width="24" height="24">
-
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/512px-Telegram_logo.svg.png?20220101141644" alt="Telegram Icon" width="24" height="24">
             '''
-            telegram_link = '<a id="header_right" href="https://t.me/nyaatorrents">' + telegram_icon_svg + '</a>'
+            telegram_link = f'<a id="header_right" href="https://t.me/nyaatorrents">{telegram_icon_svg}</a>'
             
             # Modify the header to replace GitHub with Telegram
             header = soup.find('header')
             if header:
                 # Replace the GitHub icon section with the Telegram link
-                header.find('a', {'id': 'header_right'}).replace_with(telegram_link)
+                telegram_anchor = header.find('a', {'id': 'header_right'})
+                if telegram_anchor:
+                    telegram_anchor.replace_with(telegram_link)  # Ensure it's replaced with the full Telegram link
             
             # Convert the modified HTML back to a string
             modified_html = str(soup)
